a61360dcc3b7608141e66d4148450daa47a31acf
misaki.nobuta <misaki.nobuta@digitalidentity.co.jp>
2021-11-22 01:27:41 +0900

    fa

--- main.py
+++ main.py
@@ -13,9 +13,17 @@ import os
 
 app = Flask(__name__)
 
+# ルート( / )へアクセスがあった時 --- (*1)
 @app.route("/")
-def hello():
-    return "Hello, Heroku"
-
+def root():
+    # HTMLでWebフォームを記述 --- (*2)
+    return """
+    <html><body>
+    <form action="/calc" method="post">
+      <input type="text" name="a"> ×
+      <input type="text" name="b">
+      <input type="submit" value="計算">
+    </form>
+    """
 if __name__ == '__main__':
   app.run()
