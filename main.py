092cce1ce098807d8a53b1948ca9e8d72ea296dc
misaki.nobuta <misaki.nobuta@digitalidentity.co.jp>
2021-11-22 01:24:21 +0900

    ad

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
+from flask import *
+from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+import urllib.request
+import requests
+from bs4 import BeautifulSoup
 from flask import Flask
 import os
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template
 
+
+# Flaskオブジェクトの生成
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, Heroku"
+@app.route('/')
+def index():
+  return render_template('index.html')
 
 if __name__ == '__main__':
   app.run()
