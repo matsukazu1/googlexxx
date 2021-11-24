519d9c7dbce86c5d51002da5dbf7f2746e16b8e6
misaki.nobuta <misaki.nobuta@digitalidentity.co.jp>
2021-11-25 00:40:25 +0900

    v

--- main.py
+++ main.py
@@ -95,11 +95,11 @@ def hello():
                 desc = meta['content'].strip()
         xx = desc
         xn2.append(str(xx))
-    #for ii in ganba:
-    #    html = requests.get(ii)
-    #    html.encoding = html.apparent_encoding
-    #    soup = BeautifulSoup(html.text, "html.parser")
-    #    df = soup.find_all(re.compile("^h1|h2|h3|h4|h5|h6"))
+    for ii in ganba:
+        html = requests.get(ii)
+        html.encoding = html.apparent_encoding
+        soup = BeautifulSoup(html.text, "html.parser")
+        df = soup.find_all(re.compile("^h1|h2|h3|h4|h5|h6"))
     #    for htag in df:
     #        if (r"<(h1|h2|h3|h4|h5|h6)"):
     #            i = htag
