5ed98e8db845188ed04d91fcfdf975ae967fccdd
misaki.nobuta <misaki.nobuta@digitalidentity.co.jp>
2022-02-22 22:34:26 +0900

    hennkou

--- main.py
+++ main.py
@@ -111,6 +111,9 @@ def hello():
         xx2 = str(soupz.title.string)
         xx = xx2
         xn.append(str(xx))
+        links = soupz.select("link[rel='canonical']")
+        for e in links: print(e.attrs["href"])
+        print(e.attrs["href"])
         xnx = "neko"
         xn3.append(Markup("<td>"))
         df = soupz.find_all(re.compile("^h1|h2|h3|h4|h5|h6"))
