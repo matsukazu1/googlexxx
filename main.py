7bb5ed18bec7ffe09222abca04f0944298f46830
misaki.nobuta <misaki.nobuta@digitalidentity.co.jp>
2022-02-23 01:19:12 +0900

    hennkou

--- main.py
+++ main.py
@@ -124,10 +124,10 @@ def hello():
             xxy2 = e2.attrs["href"]
         xn5.append(str(xxy2))
         #練習
-        links3 = soupz.select("meta[property='og:url']")
+        links3 = soupz.select("meta[property='og:type']")
         xxy3 = "なし"
         for e3 in links3:
-            xxy3 = e3.attrs["href"]
+            xxy3 = e3.attrs["content"]
         xn6.append(str(xxy3))
         xx2 = str(soupz.title.string)
         xx = xx2
