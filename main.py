bacdb4e545a4959537bff2abc33ec097fce29e4d
misaki.nobuta <misaki.nobuta@digitalidentity.co.jp>
2022-02-23 15:24:58 +0900

    hennkou

--- main.py
+++ main.py
@@ -169,8 +169,7 @@ def hello():
         xx2 = str(soupz.title.string)
         xx = xx2
         xn.append(str(xx))
-        j = 1
-        suuji.append(j + j)
+        suuji.append(numbers)
         xn3.append(Markup("<td>"))
         df = soupz.find_all(re.compile("^h1|h2|h3|h4|h5|h6"))
         for htag in df:
